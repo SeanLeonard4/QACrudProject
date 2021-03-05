b76079b6fcdd846542236d1b1bf53481641a3cd3
Sean Leonard <Sleonard@qa.com>
2021-03-05 18:48:46 +0000

    Repushing updated controller

--- src/main/java/com/qa/PatientController.java
+++ src/main/java/com/qa/PatientController.java
@@ -1,5 +1,37 @@
 package com.qa;
 
+import java.util.List;
+
 public class PatientController {
+	private PatientService service;
+
+	public PatientController(PatientService service) {
+		this.service = service;
+	}
+
+	public List<Patient> showAll() {
+		return service.showPatients();
+	}
+
+	public Patient addPatient(Patient patient) {
+		return service.addPatient(patient);
+	}
+
+	public Patient showByID(Long ID) {
+		return service.showPatientByID(ID);
+	}
+
+	public Patient showByName(String name) {
+		return service.showPatientByName(name);
+	}
+
+	public Patient updatePatient(Long ID, Patient newPatient) {
+		return service.updatePatient(ID, newPatient);
+	}
+
+	public Boolean deletePatient(Long ID) {
+		service.deletePatient(ID);
+		return true;
+	}
 
 }
