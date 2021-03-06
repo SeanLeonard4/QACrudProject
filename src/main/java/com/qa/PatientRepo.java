7ea7982a9586d3ed66007cf16b6524ac0369e5ff
Sean Leonard <Sleonard@qa.com>
2021-03-06 15:00:01 +0000

    Pushing updated files

--- src/main/java/com/qa/PatientRepo.java
+++ src/main/java/com/qa/PatientRepo.java
@@ -1,9 +1,15 @@
 package com.qa;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface PatientRepo extends JpaRepository<Patient, Long> {
 
 	Patient findByName(String name);
+
+	Patient save(Patient patient, LocalDate vaccineDate, LocalDate futureVacDate);
 
 }
