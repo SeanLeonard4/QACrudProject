2713b2dd97aa8e1d1d1bbe574d1348af8474eeb6
Sean Leonard <Sleonard@qa.com>
2021-03-10 15:57:56 +0000

    Unit mockito tests which passes for all CRUD functions

--- src/test/java/com/qa/service/PatientServiceDBMockitoTesting.java
+++ src/test/java/com/qa/service/PatientServiceDBMockitoTesting.java
@@ -2,6 +2,9 @@ package com.qa.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,4 +37,43 @@ public class PatientServiceDBMockitoTesting {
 		Mockito.verify(repo, Mockito.times(1)).save(newPatient);
 	}
 
+	@Test
+	void testUpdate() {
+		// GIVEN
+		// ID
+		Long id = 1L;
+		// NEW Patient DATA
+		Patient newPatient = new Patient("Sean", 23, "Sealeo123@gmail.com", "SK8 3QL", "Astrazeneca");
+		// OPTIONAL Patient
+		Optional<Patient> optPatient = Optional.of(new Patient(id, null, 0, null, null, null));
+		// UPDATED PATIENT
+		Patient updatedPatient = new Patient(id, newPatient.getName(), newPatient.getAge(), newPatient.getEmail(),
+				newPatient.getPostCode(), newPatient.getVaccine());
+		// WHEN
+		Mockito.when(repo.findById(id)).thenReturn(optPatient);
+		Mockito.when(repo.save(updatedPatient)).thenReturn(updatedPatient);
+		// THEN
+		assertThat(service.updatePatient(id, newPatient)).isEqualTo(updatedPatient);
+	}
+
+	@Test
+	void testDelete() {
+		// GIVEN
+		Long id = 1L;
+		// WHEN
+		// THEN
+		assertThat(service.deletePatient(id)).isEqualTo(true);
+	}
+
+	@Test
+	void testGet() {
+		// GIVEN
+		Long id = 1L;
+		Patient dbPatient = new Patient(id, "Sean", 23, "Sealeo123@gmail.com", "SK8 3QL", "Astrazeneca");
+		List<Patient> allPatients = List.of(dbPatient);
+		// WHEN
+		Mockito.when(repo.findAll()).thenReturn(allPatients);
+		// THEN
+		assertThat(service.showPatients()).isEqualTo(allPatients);
+	}
 }
